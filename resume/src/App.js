099b0f03802f251d66d3173ec1a225ee4beaac72
Sohaib Basit <sohaib.spiresignage@gmail.com>
2023-03-06 16:48:57 +0500

    commit

---
+++ resume/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Resume from './resume/Resume.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header title="Sohaib Basit">
+      </header>
+      <body>
+        <Resume/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
